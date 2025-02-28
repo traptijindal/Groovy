57fec192492a00a978db13c8d3a2f0e2b04426bf
traptijindal <traptijindal5678@gmail.com>
2025-03-01 00:09:24 +0530

    error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import Sidebar from '../Components/Sidebar/Sidebar.jsx'
 import Overview from '../Components/Overview/Overview.jsx'
 import "./Home.css"
-import Slider from '../Components/Slider/Slider.jsx'
 
 const Home = ({token}) => {
   return (
     <div>
       <Sidebar />
       <div className="main-content">
-      {/* <Slider token={token}/> */}
         <Overview token={token}/>
       </div>
    </div>
